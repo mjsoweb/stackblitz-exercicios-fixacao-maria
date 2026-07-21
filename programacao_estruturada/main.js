2f59e75a61acaa32b18700ade07bf30ebaf97ab9
mjsoweb <oliveira.maju@yahoo.com.br>
2026-07-21 13:25:07 -0300

    resolução exercício 1 de programação estruturada

--- programacao_estruturada/main.js
+++ programacao_estruturada/main.js
@@ -1,11 +1,11 @@
-// programacao_estruturada/app.js
+// programacao_estruturada/main.js
 
 // Importando a biblioteca que criamos no outro arquivo
-const conversores = require('./conversores');
+const conversor = require('./conversor');
 
 // Executando os testes e exibindo os resultados no console
-console.log(conversores.celsiusParaFahrenheit(30)); // Esperado: 86
-console.log(conversores.horasParaMinutos(2));        // Esperado: 120
-console.log(conversores.idadeEmDias(20));          // Esperado: 7300
-console.log(conversores.kmParaMetros(3));          // Esperado: 3000
-console.log(conversores.consumoMedio(500, 40));    // Esperado: 12.5
+console.log(conversor.celsiusParaFahrenheit(30)); // Esperado: 86
+console.log(conversor.horasParaMinutos(2));        // Esperado: 120
+console.log(conversor.idadeEmDias(20));          // Esperado: 7300
+console.log(conversor.kmParaMetros(3));          // Esperado: 3000
+console.log(conversor.consumoMedio(500, 40));    // Esperado: 12.5
